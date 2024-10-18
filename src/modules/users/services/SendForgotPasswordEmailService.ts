0ab0f216652e252b4b2530063b9438aca914ea82
Carlos-Heins <carlozheins053@gmail.com>
2024-10-18 11:07:07 -0300

    Finalização do email de redefinição de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,9 @@
 import { UsersRepository } from '../typeorm/repositories/UsersRepository'
 import AppError from '@shared/errors/AppError'
+import path from 'node:path'
 import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository'
 import EtherealMail from '@config/mail/EtherealMail'
+import { link } from 'joi'
 
 interface IRequest {
   email: string
@@ -16,6 +18,13 @@ class SendForgotPasswordEmailService {
 
     const { token } = await UserTokensRepository.generate(user.id)
 
+    const forgotPasswordTemplatePath = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs'
+    )
+
     await EtherealMail.sendMail({
       to: {
         name: user.name,
@@ -23,11 +32,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[API Vendas] Recuperação de senha',
       templateData: {
-        // biome-ignore lint/style/noUnusedTemplateLiteral: <explanation>
-        template: `Olá {{name}}: {{token}}`,
+        file: forgotPasswordTemplatePath,
         variables: {
           name: user.name,
-          token: token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
       from: {
